61a477b4573e98206aea359945ec74ed82fbf1e9
joseph.lu <guannwen@gmail.com>
2019-08-16 17:10:49 +0800

    類別的運用

--- src/main/java/com/test/AGK.java
+++ src/main/java/com/test/AGK.java
@@ -1,7 +1,14 @@
 package com.test;
 
 public class AGK {
-    public void hello(){
-        System.out.println("Hello AGK");
+
+    String agk_id = "4900290";
+    String agk_no = "a123456";
+    String agk_name = "陳偉杰";
+    int agk_star = 3;
+    String agk_stock = "2000";
+
+    public void hello(String id, String stock){
+        System.out.println(id+"'s stock=" + stock);
     }
 }
